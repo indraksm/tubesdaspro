66ddde7c85131b4748d51831076d36066233d265
unknown <abbeljatmiko@gmail.com>
2022-04-02 13:28:32 +0700

    update file

--- main.py
+++ main.py
@@ -1,52 +1,77 @@
+from tkinter import FALSE
 import pandas as pd
-df1 = pd.read_csv('user.csv')
-df2 = pd.read_csv('game.csv')
-df3 = pd.read_csv('kepemilikan.csv')
-df4 = pd.read_csv('riwayat.csv')
 
+df1 = pd.read_csv('./user.csv', sep = ";")
+df2 = pd.read_csv('./game.csv', sep = ";")
+df3 = pd.read_csv('./kepemilikan.csv', sep = ";")
+df4 = pd.read_csv('./riwayat.csv', sep = ";")
 
-end = False
-while end == False:
-    pilih = input(">>>")
-    user = []
-    passw = []
-    admin = []
-    log = False
+#Untuk syarat Register Username dan Password
+huruf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+angka = "0123456789_-"
+    
+#F02 Register
+def register(df1):
+    sama = False
+    valid = True
     jadiUser = False
-    jadiAdmin = False
+    nama = input("Masukan nama: ")
+    username = input("Masukan username: ")
+    password = input("Masukan password: ")
+
+    for i in df1["username"]:
+        if i == username:
+            sama = True
+
+    if sama == True:
+        print("Username", username, "sudah terpakai, silahkan gunakan username lain")
+
+    else:
+        for i in username:
+            if (i not in huruf) and (i not in huruf.lower()) and (i not in angka):
+                valid = False
+        if valid == False:
+            print("Username", username, "tidak valid")
+        else:
+            print("Username", username, 'telah berhasil register ke dalam "Binomo".')
+            jadiUser = True
+            if jadiUser == True:
+                role = "User"
+            
+        
+            datauser = {"username" : [username],
+                        "nama" : [nama],
+                        "password" : [password],
+                        "role" : [role],
+                        "saldo" : []
+            }
+    df1 = pd.DataFrame(data=datauser)
+
+        
+        
+        
+
+
+
+#F03 Login
+elif pilih == "login":
+    a = input("Masukkan username: ")
+    b = input("Masukkan password: ")
+    for i in range(len(user)):
+        if a == user[i] and b == passw[i]:
+            print("Halo", nama, '! Selamat datang di "Binomo" ')
+            log = True
+        else:
+            print("Password atau username salah atau tidak ditemukan")
+
+elif pilih == "mencoba_sesuatu":
+    if log == False:
+        print("Maaf, anda harus login terlebih dahulu untuk mengirim perintah selain login")
+
+elif pilih == "hanya_user":
+    if jadiUser == False:
+        print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
     
-    #F02 Register
-    if pilih == "register":
-        nama = input("Masukan nama: ")
-        username = input("Masukan username: ")
-        password = input("Masukan password: ")
-        print('Username', username, 'telah berhasil register ke dalam "Binomo" ')
-        jadiUser = True
-        user = user + [username]
-        passw = passw + [password]
-        for i in range(len(user)):
-            if username == user[i]:
-                print("Username", username, "sudah terpakai, silahkan menggunakan username lain")
-
-    #F02 Login
-    elif pilih == "login":
-        a = input("Masukkan username: ")
-        b = input("Masukkan password: ")
-        for i in range(len(user)):
-            if a == user[i] and b == passw[i]:
-                print("Halo", nama, '! Selamat datang di "Binomo" ')
-                log = True
-            else:
-                print("Password atau username salah atau tidak ditemukan")
-
-    elif pilih == "mencoba_sesuatu":
-        if log == False:
-            print("Maaf, anda harus login terlebih dahulu untuk mengirim perintah selain login")
-
-    elif pilih == "hanya_user":
-        if jadiUser == False:
-            print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
-    
-    elif pilih == "hanya_admin":
-        if jadiAdmin == False:
-            print("Maaf anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
+elif pilih == "hanya_admin":
+    if jadiAdmin == False:
+        print("Maaf anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
