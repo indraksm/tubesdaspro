c2f3739308e0a2141fae0ba9daf00778e6963dd8
unknown <abbeljatmiko@gmail.com>
2022-04-22 13:04:30 +0700

    tambahan

--- function.py
+++ function.py
@@ -12,3 +12,25 @@ def sortlist(x):
                 x[j] = x[j+1]
                 x[j+1] = sementara
 
+def csv_to_array(dir):
+    f = open(dir, 'r')
+    raw_data = f.readlines()
+    f.close()
+
+    matrix_data = [['']]
+    index_matrix = 0
+
+    for line in raw_data:
+        index_array = 0
+
+        for c in line:
+            if c == ';':
+                matrix_data[index_matrix] += ['']
+                index_array += 1
+            elif c != '\n':
+                matrix_data[index_matrix][index_array] += c
+
+        if line != raw_data[-1]: matrix_data += [['']]
+        index_matrix += 1
+
+    return matrix_data
