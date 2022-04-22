39888f6c0d0bdca6d31a316887de8122831167c4
unknown <abbeljatmiko@gmail.com>
2022-04-22 15:48:47 +0700

    tambahan

--- F03.py
+++ F03.py
@@ -1,3 +1,4 @@
+from function import *
 def Login():
     loguser = str(input("Masukan user: "))
     logpass = str(input("Masukkan pass: "))
@@ -39,44 +40,3 @@ def Login():
     else: #(successful == False):
         print('Password atau username salah atau tidak ditemukan.')
         return successful == False # Belum logged-in.
-
-def splitFunc(x): # Split 6 kata
-    first_val = ''
-    second_val = ''
-    third_val = ''
-    fourth_val = ''
-    fifth_val = ''
-    sixth_val = ''
-    i = 0
-    terminate_search = False
-
-    while (terminate_search != True): #Mendapatkan first_val (ID)
-        if (x[i] != ','):
-            first_val += x[i]
-            i += 1
-        else:
-            while (terminate_search != True): # Mendapatkan second_val (username)
-                if (x[i+1] != ','):
-                    second_val += x[i+1]
-                    i += 1
-                else:
-                    while(terminate_search != True): # Mendapatkan third_val (nama)
-                        if(x[i+2] != ','):
-                            third_val += x[i+2]
-                            i +=1
-                        else:
-                            while(terminate_search != True): # Mendapatkan fourth_val (password)
-                                if(x[i+3] != ','):
-                                    fourth_val += x[i+3]
-                                    i+=1
-                                else:
-                                    while(terminate_search != True): # Mendapatkan fifth_val (role)
-                                        if(x[i+4] != ','):
-                                            fifth_val += x[i+4]
-                                            i+=1
-                                        else:
-                                            while(terminate_search != True): # Mendapatkan sixth_val (saldo)
-                                                if(x[i+5] != ','):
-                                                    sixth_val += x[i+4+1:]
-                                                    terminate_search = True
-    return(first_val, second_val, third_val, fourth_val, fifth_val, sixth_val)
--- function.py
+++ function.py
@@ -34,3 +34,44 @@ def csv_to_array(dir):
         index_matrix += 1
 
     return matrix_data
+
+def splitFunc(x): # Split 6 kata
+    first_val = ''
+    second_val = ''
+    third_val = ''
+    fourth_val = ''
+    fifth_val = ''
+    sixth_val = ''
+    i = 0
+    terminate_search = False
+
+    while (terminate_search != True): #Mendapatkan first_val (ID)
+        if (x[i] != ','):
+            first_val += x[i]
+            i += 1
+        else:
+            while (terminate_search != True): # Mendapatkan second_val (username)
+                if (x[i+1] != ','):
+                    second_val += x[i+1]
+                    i += 1
+                else:
+                    while(terminate_search != True): # Mendapatkan third_val (nama)
+                        if(x[i+2] != ','):
+                            third_val += x[i+2]
+                            i +=1
+                        else:
+                            while(terminate_search != True): # Mendapatkan fourth_val (password)
+                                if(x[i+3] != ','):
+                                    fourth_val += x[i+3]
+                                    i+=1
+                                else:
+                                    while(terminate_search != True): # Mendapatkan fifth_val (role)
+                                        if(x[i+4] != ','):
+                                            fifth_val += x[i+4]
+                                            i+=1
+                                        else:
+                                            while(terminate_search != True): # Mendapatkan sixth_val (saldo)
+                                                if(x[i+5] != ','):
+                                                    sixth_val += x[i+4+1:]
+                                                    terminate_search = True
+    return(first_val, second_val, third_val, fourth_val, fifth_val, sixth_val)
